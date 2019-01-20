b949afed84aa0670a67b6048ad406712856bba28
uauavoice <46749632+uauavoice@users.noreply.github.com>
2019-01-21 01:01:33 +0200

    Update init.js

--- vendor/bootstrap/js/init.js
+++ vendor/bootstrap/js/init.js
@@ -1,7 +1,7 @@
 var LiveHub = ['bfaaaf84e7585c609535d0f687995326', 'live', ''];
 
 var videoHub9 = [
-['e9c85329b9d6591ed57d985b994c02fae177cd795135c5905c6d2e982a439ce5', 'vid1ep', 'HaqgqMVu'],
+['f55850365170437128286ee54d1aee554e1b4c3e9c351af8e9d3ca22b03ee166', 'vid1ep', 'HaqgqMVu'],
 ['70daf35621b9f712aaee893e920c26d69ff6c475f66d8d80f44ac67278fa3cc8', 'vid', 'c2hmeypC']
 ];
 
