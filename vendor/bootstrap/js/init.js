e6b6bd9b2bcd65f4908de226136c8aef7531543c
uauavoice <46749632+uauavoice@users.noreply.github.com>
2019-01-21 20:42:59 +0200

    Update init.js

--- vendor/bootstrap/js/init.js
+++ vendor/bootstrap/js/init.js
@@ -91,6 +91,7 @@ storedText = text.replace( "302=https:", "" );} else {
 	    $.get(storedText.replace("playlist", "chunklist_b10256000"), function(data){
  if(data.indexOf('RESOLUTION') + 1) {
 	 storedText = storedText.replace("playlist", "chunklist_b6128000");
+	 console.log(storedText);
  }
 });
 	   // storedText = storedText.replace("playlist", "chunklist_b6128000");
