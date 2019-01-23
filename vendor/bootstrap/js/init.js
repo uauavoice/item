c62f20e1bd309ace9070eceb577899560094255d
uauavoice <46749632+uauavoice@users.noreply.github.com>
2019-01-23 19:02:19 +0200

    Update init.js

--- vendor/bootstrap/js/init.js
+++ vendor/bootstrap/js/init.js
@@ -10,20 +10,20 @@ var videoHub9 = [
 ];
 
 var videoHub8 = [
-['b73c92106cf47249f3b1cb495802d10ec42b0c589575231a75d1af137a7bc5f3', 'vid1ep', 'c2hmeypC'],
-['267a0733a10e62b48f65bf2f98a0f632a76bb62348741af0464655ddb656b139', 'vid2ep', 'sCyOKKqC'],
-['45cda93ee0c7e13baa98700204ec43225c28f10f5046e1d54d1288c7ddcc4369', 'vid3ep', 'Wa3qK4Ou'],
-['234cdf74cfd4e8e3a5f8b5fc85855d6c3efecace911c0e126b4577a5629ace2d', 'vid4ep', 'ItKkJDnR'],
-['1eeabfb81bd4f5002310afe956341c1db747eddd2ffdd070c95aab9cb4233523', 'vid5ep', 'ItKkBSmR'],
-['8caf4d1ac6d2539f961ce865a6edf763e43f64f69a1ff8e9119fc96f3e6e8c10', 'vid6ep', 'L28x7jFu'],
-['344c301768be501a69cfc5862806fdc40cd79c7ba9de05b0ccd3639b4b1bc7be', 'vid7ep', 'sCN6NVsC'],
-['3c58891d3f73b99c8cab2c4dadee0335168fb0fd5a6e509805bb6823714de597', 'vid8ep', '7RzbzvoR'],
-['d7dfef1e9af4072f42d38b92a5fbbc78ca50e0a829d868a0d90ef48ce2af06dc', 'vid9ep', 'pRZF6x9t'],
-['0ce637bf50a9b393dc511082522a882b43bc3c6eb1e2e9177fb73b471e862965', 'vid10ep', 'nCc6vNna'],
-['8d66e390de1dd6c938af54783ccffefd142eb19d7f7165f568c64072c323af5b', 'vid11ep', 'E2DNc1Au'],
-['873a7109052f2e64d1a0c6601553812f49e751c40b004f724afc8ce0c7e28f1b', 'vid12ep', 'pRZFdxBt'],
-['c49b40a5264c27257ab25a4f2d82b7616308d9be4f698a76196cc79bc60e2f9c', 'vid13ep', 'KRjxmOnC'],
-['ec2f4d9da1df79e52def6c9a00f8221e1961fb9f5651cdd04c0b483fe9d1ed25', 'vid14ep', 'ku5K9xBa']
+['b73c92106cf47249f3b1cb495802d10ec42b0c589575231a75d1af137a7bc5f3', 's8ep1', 'c2hmeypC'],
+['267a0733a10e62b48f65bf2f98a0f632a76bb62348741af0464655ddb656b139', 's8ep2', 'sCyOKKqC'],
+['45cda93ee0c7e13baa98700204ec43225c28f10f5046e1d54d1288c7ddcc4369', 's8ep3', 'Wa3qK4Ou'],
+['234cdf74cfd4e8e3a5f8b5fc85855d6c3efecace911c0e126b4577a5629ace2d', 's8ep4', 'ItKkJDnR'],
+['1eeabfb81bd4f5002310afe956341c1db747eddd2ffdd070c95aab9cb4233523', 's8ep5', 'ItKkBSmR'],
+['8caf4d1ac6d2539f961ce865a6edf763e43f64f69a1ff8e9119fc96f3e6e8c10', 's8ep6', 'L28x7jFu'],
+['344c301768be501a69cfc5862806fdc40cd79c7ba9de05b0ccd3639b4b1bc7be', 's8ep7', 'sCN6NVsC'],
+['3c58891d3f73b99c8cab2c4dadee0335168fb0fd5a6e509805bb6823714de597', 's8ep8', '7RzbzvoR'],
+['d7dfef1e9af4072f42d38b92a5fbbc78ca50e0a829d868a0d90ef48ce2af06dc', 's8ep9', 'pRZF6x9t'],
+['0ce637bf50a9b393dc511082522a882b43bc3c6eb1e2e9177fb73b471e862965', 's8ep10', 'nCc6vNna'],
+['8d66e390de1dd6c938af54783ccffefd142eb19d7f7165f568c64072c323af5b', 's8ep11', 'E2DNc1Au'],
+['873a7109052f2e64d1a0c6601553812f49e751c40b004f724afc8ce0c7e28f1b', 's8ep12', 'pRZFdxBt'],
+['c49b40a5264c27257ab25a4f2d82b7616308d9be4f698a76196cc79bc60e2f9c', 's8ep13', 'KRjxmOnC'],
+['ec2f4d9da1df79e52def6c9a00f8221e1961fb9f5651cdd04c0b483fe9d1ed25', 's8ep14', 'ku5K9xBa']
 ];
 
 
@@ -48,7 +48,6 @@ if (!chekuparm) {
 }
 switch (chekuparm) {
   case 's9ep1':
-    console.log('1ep');
 sylka =videoHub9[0][0];
 tapevid=videoHub9[0][1];
 iframeidz=videoHub9[0][2];
@@ -56,19 +55,112 @@ $('#Livetitl').html("Golos 9 Sezon<small>  / 1 Выпуск от 20.01.2019 </sm
 $('#Gtitl').html("Golos 9 Sezon / 1 Выпуск от 20.01.2019");
     break;
 case 's9ep2':
-   // console.log('1ep');
 sylka =videoHub9[1][0];
 tapevid=videoHub9[1][1];
 iframeidz=videoHub9[1][2];
 $('#Livetitl').html("Golos 9 Sezon<small>  / 2 Выпуск от 27.01.2019 - Анонс </small>");
 $('#Gtitl').html("Golos 9 Sezon / Анонс 2 Выпуск от 27.01.2019");
     break;
+case 's8ep1':
+sylka =videoHub8[0][0];
+tapevid=videoHub8[0][1];
+iframeidz=videoHub8[0][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 1 Выпуск от 28.01.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 1 Выпуск от 28.01.2018");
+break;
+case 's8ep2':
+sylka =videoHub8[1][0];
+tapevid=videoHub8[1][1];
+iframeidz=videoHub8[1][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 2 Выпуск от 04.02.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 2 Выпуск от 04.02.2018");
+break;
+case 's8ep3':
+sylka =videoHub8[2][0];
+tapevid=videoHub8[2][1];
+iframeidz=videoHub8[2][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 3 Выпуск от 11.02.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 3 Выпуск от 11.02.2018");
+break;
+case 's8ep4':
+sylka =videoHub8[3][0];
+tapevid=videoHub8[3][1];
+iframeidz=videoHub8[3][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 4 Выпуск от 18.02.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 4 Выпуск от 18.02.2018");
+break;		
+case 's8ep5':
+sylka =videoHub8[4][0];
+tapevid=videoHub8[4][1];
+iframeidz=videoHub8[4][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 5 Выпуск от 25.02.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 5 Выпуск от 25.02.2018");
+break;	
+case 's8ep6':
+sylka =videoHub8[5][0];
+tapevid=videoHub8[5][1];
+iframeidz=videoHub8[5][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 6 Выпуск от 04.03.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 6 Выпуск от 04.03.2018");
+break;	
+case 's8ep7':
+sylka =videoHub8[6][0];
+tapevid=videoHub8[6][1];
+iframeidz=videoHub8[6][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 7 Выпуск от 11.03.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 7 Выпуск от 11.03.2018");
+break;	
+case 's8ep8':
+sylka =videoHub8[7][0];
+tapevid=videoHub8[7][1];
+iframeidz=videoHub8[7][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 8 Выпуск от 18.03.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 8 Выпуск от 18.03.2018");
+break;			
+case 's8ep9':
+sylka =videoHub8[8][0];
+tapevid=videoHub8[8][1];
+iframeidz=videoHub8[8][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 9 Выпуск от 25.03.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 9 Выпуск от 25.03.2018");
+break;	
+case 's8ep10':
+sylka =videoHub8[9][0];
+tapevid=videoHub8[9][1];
+iframeidz=videoHub8[9][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 10 Выпуск от 01.04.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 10 Выпуск от 01.04.2018");
+break;			
+case 's8ep11':
+sylka =videoHub8[10][0];
+tapevid=videoHub8[10][1];
+iframeidz=videoHub8[10][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 11 Выпуск от 08.04.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 11 Выпуск от 08.04.2018");
+break;			
+case 's8ep12':
+sylka =videoHub8[11][0];
+tapevid=videoHub8[11][1];
+iframeidz=videoHub8[11][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 12 Выпуск от 15.04.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 12 Выпуск от 15.04.2018");
+break;	
+case 's8ep13':
+sylka =videoHub8[12][0];
+tapevid=videoHub8[12][1];
+iframeidz=videoHub8[12][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 13 Выпуск от 22.04.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 13 Выпуск от 22.04.2018");
+break;		
+case 's8ep14':
+sylka =videoHub8[13][0];
+tapevid=videoHub8[13][1];
+iframeidz=videoHub8[13][2];
+$('#Livetitl').html("Golos 8 Sezon<small>  / 14 Выпуск от 29.04.2018 </small>");
+$('#Gtitl').html("Golos 8 Sezon / 14 Выпуск от 29.04.2018");
+break;		
 		
-
-  case 's1ep11':
-    console.log('11ep');
-    break;
-
+		
 }
 
 
