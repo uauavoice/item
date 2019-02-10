8ffd4f9e58b8cb5e88e4d58ea6910c4e88e486db
uauavoice <46749632+uauavoice@users.noreply.github.com>
2019-02-10 20:37:16 +0200

    Update init.js

--- vendor/bootstrap/js/init.js
+++ vendor/bootstrap/js/init.js
@@ -2,7 +2,7 @@ var firstinit =true;
 var ServNot = ['k2301', 'k2303', 'k2305','k2307','k2309','k2311'];
 var ServGood = ['k2302', 'k2304', 'k2306','k2308','k2310','k2312'];
 var randoms= Math.floor(Math.random() * 6);
-var LiveHub = ['5d4c95b607252fbbb2acfe7b351bd8e6', 'live', ''];
+var LiveHub = ['a1c4b13da69d6133ae6b24d6329c4aa0', 'live', ''];
 var videoHub9 = [
 ['f55850365170437128286ee54d1aee554e1b4c3e9c351af8e9d3ca22b03ee166', 's9ep1', 'HaqgqMVu'],
 ['6e4a1bd51f8c4301a87282784cec0bb65f53b7e35b6ce47eed684e1b70f34034', 's9ep2', 'fCQS4G0t'],
