749499304784dc71ed1206411e4788344498f088
uauavoice <46749632+uauavoice@users.noreply.github.com>
2019-06-16 20:37:02 +0300

    Update init.js

--- vendor/bootstrap/js/init.js
+++ vendor/bootstrap/js/init.js
@@ -8,7 +8,7 @@ var kidsvideoHub5  = [
 ['982c13ebc9070d23dbbb530dc9bfce3259237bd5d35d17039732843d5b2d2001', 'kids5ep1', 'TR9z80Qa'],
 ['51b7c0eaa1ae2450a298c41d97cd6cdef71423012bc3a1d3380512f996e1360a', 'kids5ep2', '9Cm0UmEu'],
 ['e22e3b52c633538128bb38119b00ae8867bc6023a5a87f386cb6063eace62952', 'kids5ep3', 'VC6PLVYt'],
-['yt', 'kids5ep4', 'PLcpkKchW7Xe74NDZxBiXENhL_AXIz_bEL']
+['yt', 'kids5ep4', 'PLcpkKchW7Xe655mI5WYkRQTfXTVWMamXk']
 ];
 
 
