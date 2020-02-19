d2e5109b7b72e6844e5f0ea55fff47361ba969e4
uauavoice <46749632+uauavoice@users.noreply.github.com>
2020-02-19 17:12:52 +0200

    Update init2.js

--- vendor/bootstrap/js/init2.js
+++ vendor/bootstrap/js/init2.js
@@ -9,7 +9,8 @@ var videoHub10 = [
 ['202001/8/88/88d1f9ce1a21a74ed01142a3af4c9f4f1f44bea340b6d3f6d1fbb328cc8ceb16', 's10ep2', 'oCGSj67C'],
 ['202002/6/69/692eb118a160c7bf3e235740e346c9505f36ef6b95d3cdfef921d01cb8923433', 's10ep3', 'oCbEVhU2'],
 ['202002/8/80/80f4e20ccaafb97e73d559cb6d37775da9638f0a35d6c3ce5896515cccbcd236', 's10ep4', 'F2e7Gnma'],
-['202002/6/60/60a3e16043db0323ea83c06aacd64ddb22078119598983ad6a12e13e6c384de7', 's10ep5', 'sRNIlvsu']
+['202002/6/60/60a3e16043db0323ea83c06aacd64ddb22078119598983ad6a12e13e6c384de7', 's10ep5', 'sRNIlvsu'],
+['202002/8/8d/8d393babf8200267e005a27b6af59dea19ddf9b630b587a8606ec8bc9bb42376', 's10ep6', 'Ktj7vNku']
 ];
 
 var kidsvideoHub5  = [
@@ -348,6 +349,13 @@ tapevid=videoHub10[4][1];
 iframeidz=videoHub10[4][2];
 $('#Livetitl').html("Golos 10 Sezon<small>  / 5 Выпуск от 16.02.2020  </small>");
 $('#Gtitl').html("Golos 10 Sezon / 5 Выпуск -  от 16.02.2020  ");
+break;
+ case 's10ep6':
+sylka =videoHub10[5][0];
+tapevid=videoHub10[5][1];
+iframeidz=videoHub10[5][2];
+$('#Livetitl').html("Golos 10 Sezon<small>  / 6 Выпуск от 23.02.2020  - Анонс</small>");
+$('#Gtitl').html("Golos 10 Sezon / 6 Выпуск -  от 23.02.2020 - Анонс ");
 break;
  case 'kids5ep1':
 sylka =kidsvideoHub5[0][0];
