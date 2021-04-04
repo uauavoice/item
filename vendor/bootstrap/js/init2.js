05e04725ddf2954f68ad3a3ba842718765825f60
uauavoice <46749632+uauavoice@users.noreply.github.com>
2021-04-05 01:11:31 +0300

    Update init2.js

--- vendor/bootstrap/js/init2.js
+++ vendor/bootstrap/js/init2.js
@@ -14,7 +14,8 @@ var videoHub11 = [
 ['202103/d/d1/d110bd566d664931443d74eda62acff3d6442063d1ab2f82c55b875e1ec19129', 's11ep7', 'nCc3DYiC'],
 ['202103/e/e6/e6809933704f7cc1dc44b92f2cd5d2299df10cde4caf912d6aa7586b9dc8fead', 's11ep8', 'H2qkJNg2'],
 ['202103/2/2a/2afadf539b4625cfbf73f130145c31c4b89cd8cf1d7fed226fcc7272fe0859d3', 's11ep9', 'VR6KFjg2'],
-['202103/f/f5/f5d84f1b64e5df03373cc7352391c8271fb2b271aeedde6dc404d200adce28de', 's11ep10', 'duLAQxGC']	
+['202103/f/f5/f5d84f1b64e5df03373cc7352391c8271fb2b271aeedde6dc404d200adce28de', 's11ep10', 'duLAQxGC'],
+['202103/f/f5/f5d84f1b64e5df03373cc7352391c8271fb2b271aeedde6dc404d200adce28de', 's11ep11', 'qC0z5krt']
 ];
 
 var videoHub10 = [
@@ -406,6 +407,13 @@ tapevid=videoHub11[9][1];
 iframeidz=videoHub11[9][2];
 $('#Livetitl').html("Golos 11 Sezon<small>  / 10 Выпуск от 28.03.2021 </small>");
 $('#Gtitl').html("Golos 11 Sezon / 10 Выпуск -  от 28.03.2021 ");
+break;
+ case 's11ep11':
+sylka =videoHub11[10][0];
+tapevid=videoHub11[10][1];
+iframeidz=videoHub11[10][2];
+$('#Livetitl').html("Golos 11 Sezon<small>  / 11 Выпуск от 04.04.2021 </small>");
+$('#Gtitl').html("Golos 11 Sezon / 11 Выпуск -  от 04.04.2021 ");
 break;
  case 's10ep1':
 sylka =videoHub10[0][0];
